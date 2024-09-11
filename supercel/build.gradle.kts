b09a5effb71231718b7c993344f9314f15846b02
Ian Rumac <ian.rumac@outlook.com>
2024-09-11 16:44:19 +0200

    Add publish action

--- supercel/build.gradle.kts
+++ supercel/build.gradle.kts
@@ -1,16 +1,26 @@
+import groovy.json.JsonBuilder
+
+buildscript {
+    extra["awsAccessKeyId"] = System.getenv("AWS_ACCESS_KEY_ID") ?: findProperty("aws_access_key_id")
+    extra["awsSecretAccessKey"] = System.getenv("AWS_SECRET_ACCESS_KEY") ?: findProperty("aws_secret_access_key")
+    extra["sonatypeUsername"] = System.getenv("SONATYPE_USERNAME") ?: findProperty("sonatype_username")
+    extra["sonatypePassword"] = System.getenv("SONATYPE_PASSWORD") ?: findProperty("sonatype_password")
+}
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     id("maven-publish")
+    id("signing")
 }
 
+version = "0.1.3"
 android {
     namespace = "com.superwall.supercel"
     compileSdk = 34
 
     defaultConfig {
         minSdk = 26
-        targetSdk = 34
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
@@ -47,13 +57,9 @@ android {
 dependencies {
     implementation("net.java.dev.jna:jna:5.14.0@aar")
     implementation(libs.coroutines)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
 
 afterEvaluate {
-
     publishing {
         publications {
             create<MavenPublication>("release") {
@@ -61,7 +67,68 @@ afterEvaluate {
                 groupId = "com.superwall.supercel"
                 artifactId = "supercel"
                 version = "0.1.3" // Set your library version
+
+                pom {
+                    name.set("SuperCEL")
+                    description.set("Superwall CEL Evaluator")
+                    url.set("https://superwall.com")
+
+                    licenses {
+                        license {
+                            name.set("MIT License")
+                            url.set("https://github.com/superwall/Superwall-Android?tab=MIT-1-ov-file#")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("ianrumac")
+                            name.set("Ian Rumac")
+                            email.set("ian@superwall.com")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:git@github.com:superwall/SuperCEL-Android.git")
+                        developerConnection.set("scm:git:ssh://github.com:superwall/SuperCEL-Android.git")
+                        url.set("scm:git:https://github.com/superwall/SuperCEL-Android.git")
+                    }
+                }
+            }
+        }
+
+        repositories {
+            mavenLocal()
+
+            val sonatypeUsername: String? by extra
+            val sonatypePassword: String? by extra
+
+            if (sonatypeUsername != null && sonatypePassword != null) {
+                maven {
+                    url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                    credentials(PasswordCredentials::class.java) {
+                        username = sonatypeUsername
+                        password = sonatypePassword
+                    }
+                }
             }
         }
     }
+
+
+    signing {
+        val signingKeyId: String? = System.getenv("SIGNING_KEY_ID")
+        val signingPassword: String? = System.getenv("SIGNING_PASSWORD")
+        val signingSecretKeyRingFile: String? = System.getenv("SIGNING_SECRET_KEY_RING_FILE")
+        useInMemoryPgpKeys(signingKeyId, signingSecretKeyRingFile, signingPassword )
+        sign(publishing.publications["release"])
+    }
 }
+
+tasks.register("generateBuildInfo") {
+    doLast {
+        var buildInfo = mapOf("version" to version)
+        val jsonOutput = JsonBuilder(buildInfo).toPrettyString()
+        val outputFile = File("${getLayout().buildDirectory}/version.json")
+        outputFile.writeText(jsonOutput)
+    }
+}
+
