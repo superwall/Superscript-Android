2689806e8077357f1b718b007f50a59b0c559069
Ian Rumac <ian.rumac@outlook.com>
2025-03-19 16:55:46 +0100

    Update ABI filters

--- supercel/build.gradle.kts
+++ supercel/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
-            abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86_64")
+            abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86_64", "x86")
         }
 
     }
